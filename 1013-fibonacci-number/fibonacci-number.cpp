2009c975807f1018a00e8e382713bf586529a4e9
Dannel Mulja <101443060+dannelmlj@users.noreply.github.com>
2025-05-05 06:02:26 +0800

    Time: 0 ms (100.00%) | Memory: 8.2 MB (17.34%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1){
+            return n;
+        }
+        vector<int> fibs(n+1);
+        fibs[0] = 0;
+        fibs[1] = 1;
+        for (int i=2; i<=n; i++){
+            fibs[i] = fibs[i-2] + fibs[i-1];
+        }
+        return fibs[n-1] + fibs[n-2];
+    }
+};
