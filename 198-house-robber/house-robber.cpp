11bd1505e4989fff8111541f449b0549a0c5881e
Dannel Mulja <101443060+dannelmlj@users.noreply.github.com>
2025-05-06 02:07:06 +0800

    Time: 0 ms (100.00%) | Memory: 10.8 MB (6.14%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() == 1){
+        if (nums.size() <= 1){
             return nums[0];
         }
         if (nums.size() == 2){
-            if (nums[0] > nums[1]){
-                return nums[0];
-            }
-            return nums[1];
+            return max(nums[0], nums[1]);
         }
-        if (nums.size() == 3){
-            if (nums[1] > (nums[0]+nums[2])){
-                return nums[1];
-            }
-            return nums[0]+nums[2];
+        vector<int> dp(nums.size());
+
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = nums[0] + nums[2];
+
+        for (int i=3; i<nums.size(); i++){
+            dp[i] = max(nums[i] + dp[i-2], nums[i] + dp[i-3]);
         }
 
-        vector<int> outs;   
-        for (int i = 0 ; i<nums.size(); i++){
-            outs.push_back(nums[i]);
-        }
-        outs[2] = outs[0] + outs[2];
-        for (int j=3; j<nums.size();j++){
-            int first = outs[j] + outs[j-3];
-            int second = outs[j] + outs[j-2];
+        return max(dp[nums.size()-2], dp[nums.size()-1]);
 
-            if (first>second){
-                outs[j] += outs[j-3];
-            }
-            else{
-                outs[j] += outs[j-2];
-            }
-        }
-        if (outs[outs.size()-1] > outs[outs.size()-2]){
-            return outs[outs.size()-1];
-        }
-        return outs[outs.size()-2];
+              
     }
-
 };
